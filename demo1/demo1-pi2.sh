f59dbf70531ac8cfe5eb17e7f4cef1e7da7a2d4b
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-06-07 11:31:04 +0800

    match paper file names

--- demo1/demo1-pi2.sh
+++ demo1/demo1-pi2.sh
@@ -5,12 +5,12 @@ when=`date -Iseconds`
 cd kyber
 git checkout a621b8dde405cc507cbcfc5f794570a4f98d69cc
 cd ref
-cp ../../kyberslash-pi2.c .
+cp ../../demo1-pi2.c .
 gcc \
 -Os -g \
 -Wall -Wextra -Wpedantic -Wmissing-prototypes -Wredundant-decls -Wshadow -Wpointer-arith \
 -DKYBER_K=2 \
 kex.c kem.c indcpa.c polyvec.c poly.c ntt.c cbd.c reduce.c verify.c fips202.c symmetric-shake.c randombytes.c \
-kyberslash-pi2.c -o kyberslash
+demo1-pi2.c -o kyberslash
 time ./kyberslash > kyberslash.out.$when
 tail -1 kyberslash.out.$when
